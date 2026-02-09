716d5aa5fec5599c591d0499cb0679e9e4e394a1
John-Dav9 <tchomguijohn@gmail.com>
2026-02-09 22:04:39 +0100

    chore: set prod apiBase for render

---
+++ frontend/autosales-web/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  apiBase: 'https://bmw-sales-analysis.onrender.com',
+  firebase: {
+    apiKey: 'AIzaSyCyNkOiXzA1A-GMAmjR9TzN38Oq54DB9C0',
+    authDomain: 'bmw-sales-analysis-b36c6.firebaseapp.com',
+    projectId: 'bmw-sales-analysis-b36c6',
+    storageBucket: 'bmw-sales-analysis-b36c6.firebasestorage.app',
+    messagingSenderId: '492575012320',
+    appId: '1:492575012320:web:ffbbc40e55412a97442785',
+    measurementId: 'G-XKP0YM62D1'
+  }
+};
